01d9bb5d23be5033dc6c73829e81d5bb8b9dce7d
Gledson Cruz <gledsoncruz@Gledsons-MacBook-Air.local>
2026-03-23 14:15:55 -0300

    first commit

---
+++ NX3000/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NX3000
+//
+//  Created by Gledson Cruz on 23/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NX3000/NX3000App.swift
@@ -0,0 +1,17 @@
+//
+//  NX3000App.swift
+//  NX3000
+//
+//  Created by Gledson Cruz on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct NX3000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
